0bea57e8eeb9adf17eae0d3127e8ddd605e80975
Atanu <mightyapon98@gmail.com>
2022-08-01 23:04:01 +0600

    Lesson 03

---
+++ Lesson~03/Data Type/Datatype.js
@@ -0,0 +1,20 @@
+let ans="It's a cat";
+//ans='it's '
+
+// Array //
+const fruit = ['Apple','Mango','Orange'];
+fruit[0]='Banana';
+console.log(fruit);
+document.getElementById('demo').innerHTML=fruit;
+
+
+// JAVA SCRIPT OBJECT //
+const person =
+{
+   firstName:'Atanu',
+   lastName: 'Saha',
+   Age: 25
+
+};
+
+console.log(person.Age +''+person.firstName+ ''+person.lastName);
